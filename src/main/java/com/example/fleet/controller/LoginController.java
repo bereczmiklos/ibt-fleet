48a10c6a538f56ed1c5a1bfda3d7c8e71dad8e96
Berecz Miklós <bereczmiklos@stud.uni-obuda.hu>
2022-02-14 14:14:16 +0100

    login controller added

---
+++ src/main/java/com/example/fleet/controller/LoginController.java
@@ -0,0 +1,26 @@
+package com.example.fleet.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import java.lang.annotation.Retention;
+
+@Controller
+public class LoginController {
+
+    @PostMapping("/login")
+    public String login(@RequestParam(name = "username") String userName, Model model){
+
+        model.addAttribute(RentController.CLIENT_ID, "1");
+        return "main";
+    }
+
+    @GetMapping("/logout")
+    public String logout(Model model){
+        model.addAttribute(RentController.CLIENT_ID,null);
+        return null;
+    }
+}
