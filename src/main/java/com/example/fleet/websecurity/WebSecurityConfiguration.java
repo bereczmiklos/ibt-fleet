bc82c8ddee8ec28138b7063916b15f2fcb13f237
Berecz Miklós <bereczmiklos@stud.uni-obuda.hu>
2022-03-28 16:00:29 +0200

    WebSecurity modified

--- src/main/java/com/example/fleet/websecurity/WebSecurityConfiguration.java
+++ src/main/java/com/example/fleet/websecurity/WebSecurityConfiguration.java
@@ -23,11 +23,11 @@ public class WebSecurityConfiguration extends WebSecurityConfigurerAdapter {
     protected void configure(HttpSecurity http) throws Exception {
         http
                 .authorizeRequests()
-                .antMatchers("/", "/home").permitAll()
+                .antMatchers("/", "/login").permitAll()
                 .anyRequest().authenticated()
                 .and()
                 .formLogin()
-                .loginPage("/login")
+                //.loginPage("/login")
                 .permitAll()
                 .and()
                 .logout()
@@ -40,7 +40,7 @@ public class WebSecurityConfiguration extends WebSecurityConfigurerAdapter {
         UserDetails user =
                 User.withDefaultPasswordEncoder()
                         .username("user")
-                        .password("123")
+                        .password("password")
                         .roles("USER")
                         .build();
 
